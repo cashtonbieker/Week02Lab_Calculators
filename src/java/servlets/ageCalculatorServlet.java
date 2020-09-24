fcfdf6638c6d44fa291f608ee98cbc1ba4f7ab43
Cashton <Cashton@10.0.1.112>
2020-09-24 13:00:08 -0500

    Age Calc complete Finished coding the age calculator

---
+++ src/java/servlets/ageCalculatorServlet.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Cashton
+ */
+public class ageCalculatorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+                .forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String currentageString = request.getParameter("current_age");
+        
+        int currentage;
+        
+        try {
+            currentage = Integer.parseInt(currentageString);
+            if (currentage <= 0){
+                request.setAttribute("errorMsg", "Please enter an age greater than 0");
+                
+                getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+                        .forward(request, response);
+                return;
+            }
+            currentage += 1;
+            request.setAttribute("result", currentage);
+        }
+        catch (NumberFormatException e){
+            request.setAttribute("errorMsg", "Please enter valid age");
+        }
+       
+        
+
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+                        .forward(request, response);
+        
+         
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
